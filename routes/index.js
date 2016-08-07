12eeacacd4cdf34a0d2905cd852416f0454087c0
kunagpal <kunagpal@gmail.com>
2016-08-08 01:54:12 +0530

    Fine tuned ESLint configuration, made corresponding code changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
