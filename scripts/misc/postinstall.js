c94bd15ce752ea572cce4403cba598e508eaec7c
kunagpal <kunagpal@gmail.com>
2016-09-09 04:50:58 +0530

    Postinstall script for Windows compatibility

---
+++ scripts/misc/postinstall.js
@@ -0,0 +1,14 @@
+var fs = require('fs'),
+	path = require('path'),
+	childProcess = require('child_process'),
+
+	bcryptPath = require(path.join(__dirname, '..', '..', 'node_modules', 'bcrypt'));
+
+if (process.platform === 'win32') { // the bcrypt issue is only prevalent on Microsoft Windows
+	console.info('Running on a Windows platform, installing bcryptjs instead of bcrypt');
+	childProcess.exec('npm i bcryptjs', function () {
+		fs.rename(`${bcryptPath}s`, bcryptPath, function () {
+			console.info('bcryptjs has been installed to replace bcrypt');
+		});
+	});
+}
