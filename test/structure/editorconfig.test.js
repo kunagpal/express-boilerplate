42957f49aaefdc2698dd31c558be8c94003f1030
Kunal Nagpal <kunagpal@gmail.com>
2016-10-30 06:56:14 +0530

    Added editorconfig tests

---
+++ test/structure/editorconfig.test.js
@@ -0,0 +1,32 @@
+var assert = require('assert'),
+	editorconfig = require('editorconfig'),
+
+	TAB_WIDTH = 4;
+
+describe('.ignore files', function () {
+	var config = editorconfig.parseSync('.editorconfig'); // eslint-disable-line no-sync
+
+	it('should have a tab_width of 4', function () {
+		assert(config.tab_width === TAB_WIDTH, 'Tab width set to non 4 value!');
+	});
+
+	it('should have a charset of utf-8', function () {
+		assert(config.charset === 'utf-8', 'Charset set to non "utf-8" value!');
+	});
+
+	it('should have an indent_size of 4', function () {
+		assert(config.indent_size === TAB_WIDTH, 'Indent size set to non 4 value!');
+	});
+
+	it('should have an indent_style of 4', function () {
+		assert(config.indent_style === 'tab', 'Indent style set to non "tab" value!');
+	});
+
+	it('should have a falsy insert_final_newline value', function () {
+		assert(config.insert_final_newline === false, 'Tab width set to non truthy value!');
+	});
+
+	it('should have a truthy trim_trailing_whitespace', function () {
+		assert(config.trim_trailing_whitespace === true, 'Tab width set to falsy value!');
+	});
+});
