52857c391582b93c3e426ada65db0993c1a079c2
Kunal Nagpal <kunagpal@gmail.com>
2016-12-14 03:06:39 +0400

    :art: Made purge script require-friendly

--- scripts/database/purge.js
+++ scripts/database/purge.js
@@ -3,16 +3,23 @@ var mongodb = require('mongodb').MongoClient;
 if (process.env.NODE_ENV) {
 	throw new Error('Database purge will not occur on production environments');
 }
-mongodb.connect(process.env.MONGO, function (err, db) {
-	if (err) {
-		throw err;
-	}
 
-	db.dropDatabase(function (error) {
-		if (error) {
-			throw error;
+module.exports = function (done) {
+	mongodb.connect(process.env.MONGO, function (err, db) {
+		if (err) {
+			return done(err);
 		}
 
-		console.info(`Successfully purged database at ${process.env.MONGO}`);
+		db.dropDatabase(function (error) {
+			if (error) {
+				return done(error);
+			}
+
+			db.close();
+			console.info(`Successfully purged database at ${process.env.MONGO}`);
+			done();
+		});
 	});
-});
+};
+
+!module.parent && module.exports(process.exit);
