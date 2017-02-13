26d5ba0d488ab3b6c629fa52d19c153040d02ebd
Kunal Nagpal <kunagpal@gmail.com>
2017-02-14 04:40:14 +0530

    Added util helper to load all modules in a directory

--- utils/misc/index.js
+++ utils/misc/index.js
@@ -1,15 +1,18 @@
 /**
- * @file Contains miscellaneous helpers used throughout the project.
- */
+* @file Contains miscellaneous helpers used throughout the project.
+*/
 
-var _ = require('lodash'),
+var fs = require('fs'),
+	path = require('path'),
+
+	_ = require('lodash'),
 
 	REQUIRED_VARS = ['GOOGLE_ID', 'GOOGLE_KEY', 'FACEBOOK_ID', 'FACEBOOK_KEY', 'COOKIE_SECRET', 'SESSION_SECRET',
 		'SENTRY_DSN', 'MONGO_URI', 'PORT'];
 
 /**
- * Checks for environment sanity right before the app starts.
- */
+* Checks for environment sanity right before the app starts.
+*/
 exports.checkVars = function () {
 	var subset = _(process.env).pick(REQUIRED_VARS).keys().value(),
 		missingVars = _.difference(REQUIRED_VARS, subset).toString();
@@ -17,4 +20,17 @@ exports.checkVars = function () {
 	if (!_.isEmpty(missingVars)) {
 		throw new Error(`${missingVars.toString()} environment variables are missing!`);
 	}
+};
+
+/**
+ * Helper function to load all modules from a directory.
+ *
+ * @param   {String} dir - The directory from which to load modules.
+ * @returns {Object}     A set of all the required modules in dir.
+ */
+exports.requireDir = function (dir) {
+	return _.isString(dir) && !_.isEmpty(dir)
+		? _.transform(fs.readdirSync(path.resolve(dir)), function (result, value) { // eslint-disable-line no-sync
+			result[path.parse(value).name] = require(path.resolve(dir, value)); // eslint-disable-line global-require
+		}, {}) : {};
 };
