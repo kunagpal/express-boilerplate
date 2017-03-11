981e9b256acff116708fd42fa8e39bafd790b65b
Kunal Nagpal <kunagpal@gmail.com>
2017-03-12 04:26:57 +0530

    Made purge available as a test global

--- test/unit/bootstrap.test.js
+++ test/unit/bootstrap.test.js
@@ -4,7 +4,8 @@ var mongo = require('mongodb').MongoClient,
 	testServer,
 	testPort = 3000,
 	rootPath = path.join(__dirname, '..', '..'),
-	projectName = require(path.join(rootPath, 'package')).name;
+	projectName = require(path.join(rootPath, 'package')).name,
+	purge = require(path.join(rootPath, 'scripts', 'database', 'purge'));
 
 before(function (done) {
 	process.env.MONGO_URI = `mongodb://127.0.0.1:27017/${_.kebabCase(projectName)}-test`;
@@ -13,6 +14,8 @@ before(function (done) {
 		if (err) { return done(err); }
 
 		global.db = database;
+		global.purge = purge;
+
 		app = require(path.join(rootPath, 'app'));
 		testServer = app.listen(testPort);
 		done();
