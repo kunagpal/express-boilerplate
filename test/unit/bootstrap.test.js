4e85a64c56499e5eac57f0fc5cb3dfb56e90db81
Kunal Nagpal <kunagpal@gmail.com>
2017-03-13 03:39:50 +0530

    Updated default db write concern to 1

--- test/unit/bootstrap.test.js
+++ test/unit/bootstrap.test.js
@@ -4,17 +4,15 @@ var mongo = require('mongodb').MongoClient,
 	testServer,
 	testPort = 3000,
 	rootPath = path.join(__dirname, '..', '..'),
-	projectName = require(path.join(rootPath, 'package')).name,
-	purge = require(path.join(rootPath, 'scripts', 'database', 'purge'));
+	projectName = require(path.join(rootPath, 'package')).name;
 
 before(function (done) {
 	process.env.MONGO_URI = `mongodb://127.0.0.1:27017/${_.kebabCase(projectName)}-test`;
 
-	mongo.connect(process.env.MONGO_URI, function (err, database) {
+	mongo.connect(process.env.MONGO_URI, { w: 1 }, function (err, database) {
 		if (err) { return done(err); }
 
 		global.db = database;
-		global.purge = purge;
 
 		app = require(path.join(rootPath, 'app'));
 		testServer = app.listen(testPort);
