b7cb5d4e05e5c7c7d05edb6d0a9278e854c6cfb4
Kunal Nagpal <kunagpal@gmail.com>
2017-04-03 05:55:16 +0530

    Fixed broken .ignore structural test

--- test/structure/ignore.test.js
+++ test/structure/ignore.test.js
@@ -5,16 +5,16 @@ describe('.ignore files', function () {
 
 	describe(gitignorePath, function () {
 		var gitignore = parseIgnore(gitignorePath),
-			ignored = ['*.log', '*.pid', '*.seed', '.env', '.grunt', '.idea/**', '.lock-wscript', '.node_repl_history',
-				'.npm', '.nyc_output', 'build/Release', 'coverage', 'lib-cov', 'logs', 'node_modules', 'npm-debug.log*',
-				'out', 'pids', 'public/bower', 'public/min'];
+			ignored = ['logs', '*.log', 'npm-debug.log*', 'pids', '*.pid', '*.seed', 'lib-cov', 'coverage',
+				'.nyc_output', '.grunt', '.lock-wscript', 'build/Release', 'node_modules', 'public/bower', '.npm',
+				'.node_repl_history', '.idea/**', '.env', 'public/min', 'out'];
 
 		it('should exist', function (done) {
 			fs.stat(gitignorePath, done);
 		});
 
 		it('should ignore all basic entities', function () {
-			assert.deepStrictEqual(_.intersection(gitignore, ignored), ignored, 'Invalid .gitignore!');
+			assert.deepEqual(_.intersection(gitignore, ignored), ignored, 'Invalid .gitignore!');
 		});
 	});
 });
