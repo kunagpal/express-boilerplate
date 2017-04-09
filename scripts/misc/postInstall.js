eb726ef52e9dcb5e12e14847a6474f2cee8dd779
Kunal Nagpal <kunagpal@gmail.com>
2017-04-09 18:03:36 +0530

    Addressed code issues in scripts

--- scripts/misc/postInstall.js
+++ scripts/misc/postInstall.js
@@ -8,10 +8,14 @@ var fs = require('fs'),
 	path = require('path'),
 	childProcess = require('child_process'), // eslint-disable-line security/detect-child-process
 
-	bcryptPath = path.join(__dirname, '..', '..', 'node_modules', 'bcrypt');
+	chalk = require('chal'),
 
+	bcryptPath = path.resolve('node_modules', 'bcrypt');
+
+// eslint-disable-next-line no-process-env
 if (process.platform === 'win32' && !process.env.APPVEYOR) { // the bcrypt issue is only prevalent on Microsoft Windows
-	console.info('Running on a Windows platform, installing bcryptjs instead of bcrypt');
+	console.info(chalk.blue.bold('Running on a Windows platform, installing bcryptjs instead of bcrypt'));
+
 	childProcess.exec('npm i bcryptjs', function () {
 		fs.rename(`${bcryptPath}s`, bcryptPath, function () {
 			console.info('bcryptjs has been installed to replace bcrypt');
