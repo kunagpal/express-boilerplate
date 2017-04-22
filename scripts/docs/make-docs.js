acd34b5f2e2b9395e2c279ca48ef23616d075006
Kunal Nagpal <kunagpal@gmail.com>
2017-04-22 07:06:02 +0530

    Completed programmatic JSDoc usage script

--- scripts/docs/make-docs.js
+++ scripts/docs/make-docs.js
@@ -1,49 +1,68 @@
 #!/usr/bin/env node
 
+/* eslint-disable no-sync */
 var fs = require('fs'),
 	path = require('path'),
+
+	chalk = require('chalk'),
+
+	CONFIG_FILE = '.jsdocrc',
 
 	dirs = ['src', 'util', 'opts'],
 	targets = ['app', 'name', 'env', 'src/scanner', 'util/logger', 'opts/args', 'fs', 'path', 'readme'];
 
 module.exports = function (done) {
+	var exitCode = 0;
+
 	// setup
+	process.argv.push('-c', CONFIG_FILE);
 	require.main.paths.push(path.resolve('./node_modules/jsdoc/'));
 	process.chdir('./node_modules/');
 
 	dirs.forEach(function (directory) {
-		fs.mkdirSync(`jsdoc/${directory}`); // eslint-disable-line no-sync
+		try {
+			fs.mkdirSync(`jsdoc/${directory}`);
+		}
+		catch (e) {} // eslint-disable-line no-empty
 	});
 
 	targets.forEach(function (target) {
 		try {
-			// eslint-disable-next-line no-sync
 			fs.symlinkSync(path.resolve(`./jsdoc/lib/jsdoc/${target}.js`), `./jsdoc/${target}`);
 		}
 		catch (e) {} // eslint-disable-line no-empty
 	});
 
-	// require(path.resolve('./jsdoc/lib/jsdoc/util/runtime')).initialize([path.resolve('./jsdoc'),
-	//	path.join(__dirname, '..', '..')]);
-
-	// @todo: Figure out how to make this accept arguments
-	require(path.resolve('./jsdoc/jsdoc.js'));
-
-	// cleanup
-	targets.forEach(function (target) {
-		try {
-			fs.unlinkSync(path.resolve(`./jsdoc/${target}`)); // eslint-disable-line no-sync
-		}
-		catch (e) {} // eslint-disable-line no-empty
-	});
-
-	dirs.forEach(function (directory) {
-		fs.rmdirSync(`jsdoc/${directory}`); // eslint-disable-line no-sync
-	});
-
-	require.main.paths.pop();
 	process.chdir('..');
-	done();
+
+	try {
+		require(path.resolve('./node_modules/jsdoc/jsdoc.js'));
+	}
+	catch (e) {
+		exitCode = 1;
+		console.error(chalk.red.bold(e));
+	}
+	finally {
+		// cleanup
+		targets.forEach(function (target) {
+			try {
+				fs.unlinkSync(path.resolve(`./jsdoc/${target}`));
+			}
+			catch (e) {} // eslint-disable-line no-empty
+		});
+
+		dirs.forEach(function (directory) {
+			try {
+				fs.rmdirSync(`jsdoc/${directory}`);
+			}
+			catch (e) {} // eslint-disable-line no-empty
+		});
+
+		require.main.paths.pop();
+		!exitCode && console.info(chalk.green.bold('Successfully wrote docs to out/docs'));
+
+		done(exitCode);
+	}
 };
 
 // ensure we run this script exports if this is a direct stdin.tty run
