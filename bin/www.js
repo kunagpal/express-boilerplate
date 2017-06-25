6d0bc3e88c83b4a48d9035fb4f3ae5ed3f81eeed
Kunal Nagpal <kunagpal@gmail.com>
2017-06-26 01:00:44 +0530

    :shirt: Fixed lint errors

--- bin/www.js
+++ bin/www.js
@@ -9,30 +9,33 @@ var path = require('path'),
 	mongodb = require('mongodb').MongoClient,
 
 	app,
-	port;
+	port,
+	mongoUri,
+	packageName;
 
 process.env.NODE_ENV ? utils.checkVars() : require('dotenv').load();
 
 process.on('SIGINT', utils.handle);
 port = Number(process.env.PORT) || 3000;
 
+// eslint-disable-next-line global-require
+packageName = _.kebabCase(process.env.npm_package_name || require(path.resolve('package')).name);
+mongoUri = process.env.MONGO_URI || `mongodb://127.0.0.1:27017/${packageName}`;
+
 /**
  * Establishes a reusable database connection to the database at MONGO_URI, and starts an HTTP server.
  *
  * @param {Function} done - The callback invoked at the end of the app start routine.
  */
-mongodb.connect(process.env.MONGO_URI ||
+mongodb.connect(mongoUri, { w: 1 }, function (err, database) {
+	if (err) { throw err; }
+
+	global.db = database;
+
 	// eslint-disable-next-line global-require
-	`mongodb://127.0.0.1:27017/${_.kebabCase(process.env.npm_package_name || require(path.resolve('package')).name)}`,
-	{ w: 1 }, function (err, database) {
-		if (err) { throw err; }
+	app = require(path.resolve('app'));
 
-		global.db = database;
-
-		// eslint-disable-next-line global-require
-		app = require(path.resolve('app'));
-
-		app.set('port', port);
-		app.on('error', utils.handle);
-		app.listen(port);
-	});
+	app.set('port', port);
+	app.on('error', utils.handle);
+	app.listen(port);
+});
