4c25ffa6455354ba4e159ac7e45748c5b4ab9790
Kunal Nagpal <kunagpal@gmail.com>
2017-06-30 02:01:06 +0530

    Shifted env var setting to unit test runner

--- scripts/test/unit.js
+++ scripts/test/unit.js
@@ -4,12 +4,16 @@
  * @file Houses the bare minimum logic to get unit tests up and running.
  */
 
+/* eslint-disable no-process-env */
 var path = require('path'),
 	chalk = require('chalk'),
 
 	run = require('./run'),
 	rootPath = path.join(__dirname, '..', '..'),
-	purge = require(path.join(rootPath, 'scripts', 'database', 'purge'));
+	purge = require(path.join(rootPath, 'scripts', 'database', 'purge')),
+
+	// eslint-disable-next-line global-require
+	name = process.env.npm_package_name || require(path.join(__dirname, '..', '..', 'package')).name;
 
 /**
  * Runs tests in the test/unit directory, without generating coverage information.
@@ -20,6 +24,10 @@ module.exports = function (done) {
 	console.info(chalk.blue.bold('Running unit tests'));
 
 	global.purge = purge;
+	process.env.NODE_ENV = 'test';
+	process.env.MONGO_URI = `mongodb://127.0.0.1/${name}-test`;
+	process.env.SESSION_SECRET = 'randomSecretString';
+
 	run('unit', done);
 };
 
