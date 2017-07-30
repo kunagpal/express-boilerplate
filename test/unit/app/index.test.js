9767a407b2b2401d948b324c5110a430535be7ed
Kunal Nagpal <kunagpal@gmail.com>
2017-07-30 18:55:14 +0530

    Added unit tests for content-type negotation

--- test/unit/app/index.test.js
+++ test/unit/app/index.test.js
@@ -3,7 +3,29 @@ describe('/ routes', function () {
 		test
 			.get('/')
 			.expect(200)
-			.expect('Content-Type', 'text/html; charset=utf-8')
+			.expect('x-frame-options', 'SAMEORIGIN')
+			.expect('strict-transport-security', 'max-age=15552000; includeSubDomains')
+			.expect('x-download-options', 'noopen')
+			.expect('x-content-type-options', 'nosniff')
+			.expect('x-xss-protection', '1; mode=block')
+			.expect('content-type', 'text/html; charset=utf-8')
+			.expect('vary', 'Accept-Encoding')
+			.expect('connection', 'close')
+			.end(done);
+	});
+
+	it('should return a not found response for an arbitrary route', function (done) {
+		test
+			.get('/random')
+			.expect(404)
+			.expect('x-frame-options', 'SAMEORIGIN')
+			.expect('strict-transport-security', 'max-age=15552000; includeSubDomains')
+			.expect('x-download-options', 'noopen')
+			.expect('x-content-type-options', 'nosniff')
+			.expect('x-xss-protection', '1; mode=block')
+			.expect('content-type', 'text/html; charset=utf-8')
+			.expect('vary', 'Accept, Accept-Encoding')
+			.expect('connection', 'close')
 			.end(done);
 	});
 });
