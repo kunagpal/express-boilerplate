ef47e47e5b8b2f005dff0a5d81a03762f2a09017
Kunal Nagpal <kunagpal@gmail.com>
2017-08-16 02:20:28 +0530

    Added unit test to check against arbitrary path access

--- test/unit/app/static.test.js
+++ test/unit/app/static.test.js
@@ -1,4 +1,20 @@
 describe('static asset routes', function () {
+	describe('security', function () {
+		it('should not supply arbitrary paths', function (done) {
+			test
+				.get('/static/../style.css')
+				.expect('x-frame-options', 'SAMEORIGIN')
+				.expect('strict-transport-security', 'max-age=15552000; includeSubDomains')
+				.expect('x-download-options', 'noopen')
+				.expect('x-content-type-options', 'nosniff')
+				.expect('x-xss-protection', '1; mode=block')
+				.expect('content-type', 'text/html; charset=utf-8')
+				.expect('vary', 'Accept, Accept-Encoding')
+				.expect('connection', 'close')
+				.expect(404, done);
+		});
+	});
+
 	describe('scripts', function () {
 		// eslint-disable-next-line no-sync
 		fs.readdirSync('public/javascripts').forEach(function (script) {
